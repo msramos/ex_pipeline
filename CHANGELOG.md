3fac3ead81a4b500a6627157813948fa4bed7a60
Marcos Ramos <rms.mrcs@gmail.com>
2022-07-31 17:48:16 -0300

    Prepare for publishing on hex.pm

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Changelog for `ex_pipeline`
+
+## v0.1.0
+
+First version!
+
+### New Features
+  - Basic pipeline building and state management
--- mix.exs
+++ mix.exs
@@ -3,14 +3,18 @@ defmodule ExPipeline.MixProject do
 
   @app :ex_pipeline
   @version "0.1.0"
+  @source_url "https://github.com/msramos/ex_pipeline"
 
   def project do
     [
       app: @app,
       version: @version,
+      description: "A simple and opinionated pipeline builder",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -21,6 +25,31 @@ defmodule ExPipeline.MixProject do
   end
 
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      source_ref: "v#{@version}",
+      canonical: "http://hexdocs.pm/ex_pipeline",
+      source_url: @source_url,
+      extras: [
+        "README.md",
+        "CHANGELOG.md": [filename: "changelog", title: "Changelog"],
+        "CODE_OF_CONDUCT.md": [filename: "code_of_conduct", title: "Code of Conduct"],
+        LICENSE: [filename: "license", title: "License"],
+        NOTICE: [filename: "notice", title: "Notice"]
+      ]
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Marcos Ramos"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url}
+    ]
   end
 end
